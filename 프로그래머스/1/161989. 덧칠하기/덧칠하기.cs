ae7d9b66a8f1a3d26617c79cb915e2ae45f2a676
Daniel Lee <114923190+jhoon8903@users.noreply.github.com>
2023-11-27 10:34:24 +0900

    [level 1] Title: 덧칠하기, Time: 0.29 ms, Memory: 31.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int n, int m, int[] section)
+    {
+        int answer= 0;
+        int Index = 0;
+
+        while (Index < section.Length)
+        {
+            int start = section[Index];
+            int end = Math.Min(start + m - 1, n);
+            
+            answer++;
+            
+            while (Index < section.Length && section[Index] <= end)
+            {
+                Index++;
+            }
+        }
+        return answer;
+    }
+}
